785dd61c1623aa299b414be5ba4e7f334d4d13a8
opbnma <70656937+opbnma@users.noreply.github.com>
2021-11-03 23:33:11 +0800

    Update block-any.sh

--- block-any.sh
+++ block-any.sh
@@ -20,39 +20,39 @@ check_release() {
     fi
 }
 
-#安装必要的软件(wget),并下载中国IP网段文件(最后将局域网地址也放进去)
-get_china_ip() {
+#安装必要的软件(wget),并下载日本IP网段文件(最后将局域网地址也放进去)
+get_japan_ip() {
 	#安装必要的软件(wget)
 	rpm --help >/dev/null 2>&1 && rpm -qa |grep wget >/dev/null 2>&1 ||yum install -y wget ipset >/dev/null 2>&1 
 	dpkg --help >/dev/null 2>&1 && dpkg -l |grep wget >/dev/null 2>&1 ||apt-get install wget ipset -y >/dev/null 2>&1
 
 	#该文件由IPIP维护更新，大约一月一次更新(也可以用我放在国内的存储的版本，2018-9-8日版)
-	[ -f china_ip_list.txt ] && mv china_ip_list.txt china_ip_list.txt.old
-	wget https://raw.githubusercontent.com/opbnma/Japan-ip/main/japan.txt
-	cat china_ip_list.txt |grep 'js-file-line">' |awk -F'js-file-line">' '{print $2}' |awk -F'<' '{print $1}' >> china_ip.txt
-	rm -rf china_ip_list.txt
-	#wget https://qiniu.wsfnk.com/china_ip.txt
+	[ -f japan.txt ] && mv japan.txt japan.txt.old
+	wget https://github.com/17mon/japan_ip_list/blob/master/japan.txt
+	cat japan.txt |grep 'js-file-line">' |awk -F'js-file-line">' '{print $2}' |awk -F'<' '{print $1}' >> japan_ip.txt
+	rm -rf japan.txt
+	#wget https://qiniu.wsfnk.com/japan_ip.txt
 
 	#放行局域网地址
-	echo "192.168.0.0/18" >> china_ip.txt
-	echo "10.0.0.0/8" >> china_ip.txt
-	echo "172.16.0.0/12" >> china_ip.txt
+	echo "192.168.0.0/18" >> japan_ip.txt
+	echo "10.0.0.0/8" >> japan_ip.txt
+	echo "172.16.0.0/12" >> japan_ip.txt
 }
 
 #只允许国内IP访问
 ipset_only_china() {
-	echo "ipset create whitelist-china hash:net hashsize 10000 maxelem 1000000" > /etc/ip-black.sh
-	for i in $( cat china_ip.txt )
+	echo "ipset create whitelist-japan hash:net hashsize 10000 maxelem 1000000" > /etc/ip-black.sh
+	for i in $( cat japan_ip.txt )
 	do
-        	echo "ipset add whitelist-china $i" >> /etc/ip-black.sh
+        	echo "ipset add whitelist-japan $i" >> /etc/ip-black.sh
 	done
-	echo "iptables -I INPUT -m set --match-set whitelist-china src -j ACCEPT" >> /etc/ip-black.sh
+	echo "iptables -I INPUT -m set --match-set whitelist-japan src -j ACCEPT" >> /etc/ip-black.sh
 	#拒绝非国内和内网地址发起的tcp连接请求（tcp syn 包）（注意，只是屏蔽了入向的tcp syn包，该主机主动访问国外资源不用影响）
 	echo "iptables  -A INPUT -p tcp --syn -m connlimit --connlimit-above 0 -j DROP" >> /etc/ip-black.sh
 	#拒绝非国内和内网发起的ping探测（不影响本机ping外部主机）
 	echo "iptables  -A INPUT -p icmp -m icmp --icmp-type 8 -j DROP" >> /etc/ip-black.sh
 	#echo "iptables -A INPUT -j DROP" >> /etc/ip-black.sh
-	rm -rf china_ip.txt
+	rm -rf japan_ip.txt
 }
 
 run_setup() {
@@ -71,7 +71,7 @@ run_setup() {
 
 main() {
 	check_release
-	get_china_ip
+	get_japan_ip
 	ipset_only_china
 
 case "$release" in
